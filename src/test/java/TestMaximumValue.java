846d8249f9aa2e307c7eb36b660619b6c995af79
sadnipk01 <sadnipkenagar01@gmail.com>
2020-03-14 14:31:30 +0530

    ADDED : Given three integers and should return maximum value test case added

--- src/test/java/TestMaximumValue.java
+++ src/test/java/TestMaximumValue.java
@@ -1,2 +1,14 @@
+import org.junit.Assert;
+import org.junit.Test;
+
 public class TestMaximumValue {
+
+    private MaximumValue maximumValue;
+
+    @Test
+    public void findingMaxValue_GivenThreeIntegers_ShouldReturnMaxValue() {
+        maximumValue = new MaximumValue();
+        Integer maximumInteger = maximumValue.getMaxInteger(7, 4, 2);
+        Assert.assertEquals((Integer) 7, maximumInteger);
+    }
 }
