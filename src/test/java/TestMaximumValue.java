7eaed9f8509df4b2163973077ec3119a73272f27
sadnipk01 <sadnipkenagar01@gmail.com>
2020-03-14 15:40:42 +0530

    ADDED : Given maximum float value at first parameter should return max value test case is added

--- src/test/java/TestMaximumValue.java
+++ src/test/java/TestMaximumValue.java
@@ -27,6 +27,13 @@ public class TestMaximumValue {
     }
 
     @Test
+    public void findingMaxFloatValue_GivenFirstParameter_ShouldReturnMaxValue() {
+        maximumValue = new MaximumValue();
+        Float maximumFloat = maximumValue.getMaxvalue(10.9f, 4.5f, 7.5f);
+        Assert.assertEquals((Float) 10.9f, maximumFloat);
+    }
+
+    @Test
     public void findingMaxValue_GivenThreeFloat_ShouldReturnMaxValue() {
         maximumValue = new MaximumValue();
         Float maximumFloat = maximumValue.getMaxvalue(4.5f, 7.5f, 10.9f);
