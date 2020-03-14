5198a1f99cd27dd8f8914cf91373f92932b05209
sadnipk01 <sadnipkenagar01@gmail.com>
2020-03-14 15:37:17 +0530

    ADDED : Given maximum value at second parameter should return max value test case is added

--- src/test/java/TestMaximumValue.java
+++ src/test/java/TestMaximumValue.java
@@ -6,23 +6,30 @@ public class TestMaximumValue {
     private MaximumValue maximumValue;
 
     @Test
-    public void findingMaxValue_GivenThreeIntegers_ShouldReturnMaxValue() {
+    public void findingMaxIntegerValue_GivenFirstParameter_ShouldReturnMaxValue() {
         maximumValue = new MaximumValue();
-        Integer maximumInteger = maximumValue.getMaxInteger(7, 4, 2);
+        Integer maximumInteger = maximumValue.getMaxvalue(7, 4, 2);
+        Assert.assertEquals((Integer) 7, maximumInteger);
+    }
+
+    @Test
+    public void findingMaxIntegerValue_GivenSecondParameter_ShouldReturnMaxValue() {
+        maximumValue = new MaximumValue();
+        Integer maximumInteger = maximumValue.getMaxvalue(4, 7, 2);
         Assert.assertEquals((Integer) 7, maximumInteger);
     }
 
     @Test
     public void findingMaxValue_GivenThreeFloat_ShouldReturnMaxValue() {
         maximumValue = new MaximumValue();
-        Float maximumFloat=maximumValue.getMaxFloat(4.5f,7.5f,10.9f);
-        Assert.assertEquals((Float)10.9f,maximumFloat);
+        Float maximumFloat = maximumValue.getMaxvalue(4.5f, 7.5f, 10.9f);
+        Assert.assertEquals((Float) 10.9f, maximumFloat);
     }
 
     @Test
     public void findingMaxValue_GivenThreeString_ShouldReturnMaxValue() {
         maximumValue = new MaximumValue();
-        String maximumFloat=maximumValue.getMaxString("Honda","Ferrari","Bmw");
-        Assert.assertEquals("Honda",maximumFloat);
+        String maximumFloat = maximumValue.getMaxvalue("Honda", "Ferrari", "Bmw");
+        Assert.assertEquals("Honda", maximumFloat);
     }
 }
