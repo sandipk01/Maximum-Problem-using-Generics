4284201bc6cd9b5963d36be88a7f65c3f9af5e0d
sadnipk01 <sadnipkenagar01@gmail.com>
2020-03-14 15:48:33 +0530

    REFACTORED : Method to finding max value using generics

--- src/main/java/MaximumValue.java
+++ src/main/java/MaximumValue.java
@@ -1,25 +1,7 @@
 public class MaximumValue {
 
-    //method to find maximum value from given 3 integer values
-    public Integer getMaxInteger(Integer firstValue, Integer secondValue, Integer thirdValue) {
-        if (firstValue.compareTo(secondValue) > 0 && firstValue.compareTo(thirdValue) > 0)
-            return firstValue;
-        else if (secondValue.compareTo(firstValue) > 0 && secondValue.compareTo(thirdValue) > 0)
-            return secondValue;
-        return thirdValue;
-    }
-
-    //method to find maximum value from given 3 float values
-    public Float getMaxFloat(Float firstValue, Float secondValue, Float thirdValue) {
-        if (firstValue.compareTo(secondValue) > 0 && firstValue.compareTo(thirdValue) > 0)
-            return firstValue;
-        else if (secondValue.compareTo(firstValue) > 0 && secondValue.compareTo(thirdValue) > 0)
-            return secondValue;
-        return thirdValue;
-    }
-
-    //method to find maximum value from given 3 strings
-    public String getMaxString(String firstValue, String secondValue, String thirdValue) {
+    //generic method to find maximum value from given 3 values
+    public <T extends Comparable> T getMaxvalue(T firstValue, T secondValue, T thirdValue) {
         if (firstValue.compareTo(secondValue) > 0 && firstValue.compareTo(thirdValue) > 0)
             return firstValue;
         else if (secondValue.compareTo(firstValue) > 0 && secondValue.compareTo(thirdValue) > 0)
