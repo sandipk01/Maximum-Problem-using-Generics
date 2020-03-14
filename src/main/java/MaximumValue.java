a6a087d002e8fa3c3a6f2323ccc271b8991d95d5
sadnipk01 <sadnipkenagar01@gmail.com>
2020-03-14 14:29:42 +0530

    ADDED : Method to comparing three integers and returns maximum number

--- src/main/java/MaximumValue.java
+++ src/main/java/MaximumValue.java
@@ -1,2 +1,11 @@
 public class MaximumValue {
+
+    //method to find maximum value from given 3 integer
+    public Integer getMaxInteger(Integer firstValue, Integer secondValue, Integer thirdValue) {
+        if (firstValue.compareTo(secondValue) > 0 && firstValue.compareTo(thirdValue) > 0)
+            return firstValue;
+        else if (secondValue.compareTo(firstValue) > 0 && secondValue.compareTo(thirdValue) > 0)
+            return secondValue;
+        return thirdValue;
+    }
 }
