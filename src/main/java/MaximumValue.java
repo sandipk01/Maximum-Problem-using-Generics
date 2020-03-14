64b555051dbae222252aa6d5fb97bde652cf5e2f
sadnipk01 <sadnipkenagar01@gmail.com>
2020-03-14 14:44:12 +0530

    ADDED : Method for findig maximum value from 3 float values

--- src/main/java/MaximumValue.java
+++ src/main/java/MaximumValue.java
@@ -1,7 +1,16 @@
 public class MaximumValue {
 
-    //method to find maximum value from given 3 integer
+    //method to find maximum value from given 3 integer values
     public Integer getMaxInteger(Integer firstValue, Integer secondValue, Integer thirdValue) {
+        if (firstValue.compareTo(secondValue) > 0 && firstValue.compareTo(thirdValue) > 0)
+            return firstValue;
+        else if (secondValue.compareTo(firstValue) > 0 && secondValue.compareTo(thirdValue) > 0)
+            return secondValue;
+        return thirdValue;
+    }
+
+    //method to find maximum value from given 3 float values
+    public Float getMaxFloat(Float firstValue, Float secondValue, Float thirdValue) {
         if (firstValue.compareTo(secondValue) > 0 && firstValue.compareTo(thirdValue) > 0)
             return firstValue;
         else if (secondValue.compareTo(firstValue) > 0 && secondValue.compareTo(thirdValue) > 0)
