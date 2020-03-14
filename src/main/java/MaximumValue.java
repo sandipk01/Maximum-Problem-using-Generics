4fccb88463390053f636d98dc49ec9a6e33c8c59
sadnipk01 <sadnipkenagar01@gmail.com>
2020-03-14 16:15:53 +0530

    ADDED : Method to call getMaxValue method internally and paremeterized constructor

--- src/main/java/MaximumValue.java
+++ src/main/java/MaximumValue.java
@@ -1,11 +1,33 @@
-public class MaximumValue {
+public class MaximumValue<T extends Comparable> {
+
+    //variables with generic type
+    private T firstValue;
+    private T secondValue;
+    private T thirdValue;
+
+    //parameter less constructor
+    public MaximumValue() {
+    }
+
+    //parameter taking three generic type values
+    public MaximumValue(T firstValue, T secondValue, T thirdValue) {
+        this.firstValue = firstValue;
+        this.secondValue = secondValue;
+        this.thirdValue = thirdValue;
+    }
 
     //generic method to find maximum value from given 3 values
-    public <T extends Comparable> T getMaxvalue(T firstValue, T secondValue, T thirdValue) {
+    public T getMaxValue(T firstValue, T secondValue, T thirdValue) {
         if (firstValue.compareTo(secondValue) > 0 && firstValue.compareTo(thirdValue) > 0)
             return firstValue;
         else if (secondValue.compareTo(firstValue) > 0 && secondValue.compareTo(thirdValue) > 0)
             return secondValue;
         return thirdValue;
     }
+
+    //method to call getMaxValue method
+    public <T extends Comparable> T testMaximum() {
+        return (T) getMaxValue(firstValue, secondValue, thirdValue);
+    }
+
 }
